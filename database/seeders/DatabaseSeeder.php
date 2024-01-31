538a3873bc480388cc38c7da971fd8619675f734
SnusnumrConceit <snusnumr1996@gmail.com@>
2024-01-31 21:04:26 +0300

    [SEED]: добавлен demo-админ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,11 @@ class DatabaseSeeder extends Seeder
             PostSeeder::class,
         ]);
 
+        User::factory()->admin()->create([
+            'name' => 'admin',
+            'email' => 'demo@admin.com',
+            'password' => bcrypt('demopass')
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
