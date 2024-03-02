afc0a5df8dea700037246530e3675f6a7cbd6d0f
SnusnumrConceit <snusnumr1996@gmail.com@>
2024-03-02 23:31:05 +0300

    FIX: использование директивы @session вместо избыточного хелпера

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin')
 
 @section('content')
-    @if(session()->has('success'))
+    @session('success')
         <div class="alert alert-success mb-4">
             <h5 class="mb-0">
                 <i class="bi bi-check-circle"></i>
                 {{ session('success') }}
             </h5>
         </div>
-    @endif
+    @endsession
 
     <div class="card">
         <div class="card-header">
